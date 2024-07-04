2dc7d1c8c9cfde38db6c279af58c6afaf5415d8d
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-07-04 15:58:06 -0700

    update 5-mixed_sum_list

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/binenv python3
+"""Import module/lib"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """returns their sum as a float."""
+    return sum(mxd_lst)
