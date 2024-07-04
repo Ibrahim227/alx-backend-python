8d613ba4d4bebbed0f082b0a69f7004abd52237d
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-07-04 15:43:27 -0700

    update 5-sum_list

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Import module/lib"""
+from typing import List
 
 
-def sum_list(input_list: float) -> float:
+def sum_list(input_list: List[float]) -> float:
     """returns their sum as a float."""
     return sum(input_list)
