292d04b0334d123c51e61dcc5c5dfee560c13e76
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-07-08 16:33:18 -0700

    update 0-basic_async_syntax

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """Import required module/lib"""
 import random
+import asyncio
 
 
 async def wait_random(max_delay: int = 10):
     """ takes in an integer argument and eventually  returns it."""
-    await rando
-    return random(max_delay)
+    delay = random.uniform(0, max_delay)
+    return delay
