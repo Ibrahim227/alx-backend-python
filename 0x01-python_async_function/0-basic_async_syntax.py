e04d4a8c24bccb91bca709d0528862dc8bc230cc
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-07-08 15:47:43 -0700

    update  0-basic_async_syntax.py

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Import required module/lib"""
+import random
+
+
+async def wait_random(max_delay: int = 10) -> int:
+    """ takes in an integer argument and eventually  returns it."""
+    r = await random(0, max_delay)
+    return r
