fe1fcf079d6d68a47038bbab07e99cac40dd7459
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-07-08 17:01:32 -0700

    update 9-basic_syntax

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay: int = 10):
+async def wait_random(max_delay: int = 10) -> float:
     """ takes in an integer argument and eventually  returns it."""
     delay = random.uniform(0, max_delay)
     return delay
