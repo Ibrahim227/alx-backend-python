4e109334c0560dc0bb6fe5870f6ac9c3dc0f7f49
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-07-09 10:44:55 -0700

    update 2-measure_runtime

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python3
 """Import required module/lib"""
 import asyncio
+import time
 from random import uniform
 from typing import Generator, List
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime() -> List[float]:
+async def measure_runtime() -> float:
     """should measure the total runtime and return it."""
-    await asyncio.gather(async_comprehension(), async_comprehension(),
-                            async_comprehension(), async_comprehension())
+    start = time()
+    async_exe = [async_comprehension() for i in range(4)]
+    result = await asyncio.gather(*async_exe)
+    end = time()
 
-
-if __name__ == "__main__":
-    import time
-    s = time.perf_counter()
-    asyncio.run(measure_runtime())
-    elapsed = time.perf_counter() - s
-    print(elapsed)
+    return end - start
