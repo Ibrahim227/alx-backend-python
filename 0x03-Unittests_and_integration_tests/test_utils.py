f6ef4cff2d2117e879c67dcb2b3a97e98db118d6
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-07-29 14:35:45 -0700

    update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 """Import Module/Lib"""
 import unittest
+import utils
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """class TestAccessNestedMap inherits from unittest.TestCase"""
-    pass
+    @parameterized.expand([])
+
+    def test_access_nested_map(self):
+        """method to test that the method returns what it is supposed to"""
+        result = utils.access_nested_map(
